422ef03edaeabd183cebd0ec4161ed531662449b
Chris Mahlke <cmahlke@esri.com>
2020-10-31 21:41:43 -0700

    Tmp updates

--- Live Feeds Health Check/LiveFeedsHealthCheck.py
+++ Live Feeds Health Check/LiveFeedsHealthCheck.py
@@ -196,23 +196,235 @@ if __name__ == "__main__":
     print("===================================================================")
     data_model_dict = QueryEngine.get_feature_counts(data_model=data_model_dict)
 
+    print("\n=================================================================")
+    print(f"Current data and time")
+    print("===================================================================")
+    timeUtilsResponse = TimeUtils.get_current_time_and_date()
+    timestamp = timeUtilsResponse["timestamp"]
+
     for key, value in data_model_dict.items():
         item_id = key
+        agol_is_valid = True
         item_is_valid = value["itemIsValid"]
         service_is_valid = value["serviceResponse"]["success"]
         layers_are_valid = value["allLayersAreValid"]
+
+        # Process Retry Count
+        retry_count = 0
+        if len(value["serviceResponse"]["retryCount"]) > 0:
+            retry_count = value["serviceResponse"]["retryCount"]["retryCount"]
+        print(f"Retry count: {retry_count}")
+
+        # Process Elapsed Time
+        elapsed_time = 0
+        # elapsed time
+        if service_is_valid:
+            # get elapsed time in seconds
+            elapsed_time = value["serviceResponse"]["response"].elapsed.total_seconds()
+        print(f"Elapsed time: {elapsed_time}")
+        # Obtain the total elapsed time and counts
+        # response time directory
+        response_time_data_dir = os.path.realpath(ROOT_DIR + r"\ResponseTimeData")
+        # Create a new directory if it does not exists
+        FileManager.create_new_folder(response_time_data_dir)
+        # path to output file
+        response_time_data_file_path = os.path.join(response_time_data_dir, item_id + "." + "json")
+        # Check file existence.
+        response_time_data_file_path_exist = FileManager.check_file_exist_by_pathlib(path=response_time_data_file_path)
+        if not response_time_data_file_path_exist:
+            # If file does not exist then create it.
+            FileManager.create_new_file(response_time_data_file_path)
+            FileManager.set_file_permission(response_time_data_file_path)
+            FileManager.save(data={
+                "id": item_id,
+                "elapsed_sums": elapsed_time,
+                "elapsed_count": 1
+            }, path=response_time_data_file_path)
+            # since it's our first entry, the average is the current elapsed time
+            elapsedTimeAverage = elapsed_time
+        else:
+            # Retrieve the elapsed time DIVIDE by count
+            response_time_data = FileManager.get_response_time_data(response_time_data_file_path)
+            # total counts
+            elapsed_times_count = response_time_data["elapsed_count"]
+            # sum of all times
+            elapsed_times_sum = response_time_data["elapsed_sums"]
+            # calculated average
+            elapsed_times_average = elapsed_times_sum / elapsed_times_count
+
+            FileManager.update_response_time_data(path=response_time_data_file_path, input_data={
+                "id": item_id,
+                "elapsed_count": elapsed_times_count + 1,
+                "elapsed_sums": elapsed_times_sum + elapsed_time
+            })
+
+        alfp_data = alfpDict.get(item_id)
+        if alfp_data is None:
+            print("Error")
+        else:
+            print("Success")
+        # 10 digit Timestamp 'seconds since epoch' containing time of last
+        # Successful Run (and Service update)
+        #feedLastUpdateTimestamp = alfp_data["lastUpdateTimestamp"]
+        # 10 digit Timestamp 'seconds since epoch' containing time of last
+        # Failed run (or Service update failure)
+        # feed_last_failure_timestamp = item["lastFailureTimestamp"]
+        # 10 digit Timestamp 'seconds since epoch' containing time of last
+        # run (having a Success, a Failure, or a No Action flag ('No Data
+        # Updates')
+        #feedLastRunTimestamp = alfp_data["lastRunTimestamp"]
+        # Average number of minutes between each successful run (or Service
+        # update)
+        #avgUpdateIntervalInMins = alfp_data["avgUpdateIntervalMins"]
+        # Average number of minutes between each run
+        #avgFeedIntervalInMins = alfp_data["avgFeedIntervalMins"]
+        #
+        #consecutive_failures_count = alfp_data["consecutiveFailures"]
+        #
+        #consecutive_failures_threshold = int(value["consecutive_failures_threshold"])
+        #
+        #alfp_code = alfp_data["lastStatus"]["code"]
+
+        statusCode = StatusManager.get_status_code("000", statusCodesDataModel)
+
+        if all([agol_is_valid, item_is_valid, service_is_valid, layers_are_valid]):
+            print("\tAGOL, Item, Service checks normal")
+
+            # 001 Check
+            # Check elapsed time between now and the last updated time of the feed
+            lastUpdateTimestampDiff = timestamp - alfp_data["lastUpdateTimestamp"]
+            # Check elapsed time between now and the last run time of the feed
+            lastRunTimestampDiff = timestamp - alfp_data["lastRunTimestamp"]
+
+            # debugging and logging
+            p_currentTimeStamp = TimeUtils.convert_from_utc_to_datetime(timestamp)
+            p_feedLastUpdateTimestamp = TimeUtils.convert_from_utc_to_datetime(alfp_data["lastUpdateTimestamp"])
+            p_lastUpdateTimestampDiff = TimeUtils.convert_from_utc_to_datetime(lastUpdateTimestampDiff)
+            print(f"Run time of script:\t\t{p_currentTimeStamp}")
+            print(f"Last update of Feed:\t\t{p_feedLastUpdateTimestamp}")
+            print(f"Last update timestamp delta:\t{p_lastUpdateTimestampDiff}")
+
+            # If the Difference exceeds the average update interval by an interval of X, flag it
+            lastUpdateTimestampDiffMinutes = lastUpdateTimestampDiff / 60
+            print(f"Last update timestamp delta:\t{lastUpdateTimestampDiffMinutes} seconds")
+            # calculate the threshold
+            avgUpdateIntThreshold = int(value["average_update_interval_factor"]) * alfp_data["avgUpdateIntervalMins"]
+            print(f"Average update interval threshold: {avgUpdateIntThreshold}")
+            if lastUpdateTimestampDiffMinutes > avgUpdateIntThreshold:
+                #value["status"]["code"] = "001"
+                statusCode = StatusManager.get_status_code("001", statusCodesDataModel)
+
+            # 002 Check
+            lastRunTimestampDiffMinutes = lastRunTimestampDiff / 60
+            print(f"Last run timestamp delta:\t{lastRunTimestampDiffMinutes} seconds")
+            # calculate the threshold
+            avgFeedIntThreshold = int(value["average_feed_interval_factor"]) * alfp_data["avgFeedIntervalMins"]
+            print(f"Average Feed Interval threshold: {avgFeedIntThreshold}")
+            if lastRunTimestampDiffMinutes > avgFeedIntThreshold:
+                #item_dict["status"]["code"] = "002"
+                statusCode = StatusManager.get_status_code("002", statusCodesDataModel)
+
+            # 003 Check
+            if alfp_data["lastStatus"]["code"] == 2:
+                if alfp_data["consecutiveFailures"] > int(value["consecutive_failures_threshold"]):
+                    #item_dict["status"]["code"] = "003"
+                    statusCode = StatusManager.get_status_code("003", statusCodesDataModel)
+
+            # 004 Check
+            if alfp_data["lastStatus"]["code"] == 3:
+                if alfp_data["consecutiveFailures"] > int(value["consecutive_failures_threshold"]):
+                    #item_dict["status"]["code"] = "004"
+                    statusCode = StatusManager.get_status_code("004", statusCodesDataModel)
+
+            # 005 Check
+            if alfp_data["lastStatus"]["code"] == 1:
+                if alfp_data["consecutiveFailures"] > int(value["consecutive_failures_threshold"]):
+                    #item_dict["status"]["code"] = "005"
+                    statusCode = StatusManager.get_status_code("005", statusCodesDataModel)
+
+            # 006 Check
+            if alfp_data["lastStatus"]["code"] == -1:
+                #item_dict["status"]["code"] = "006"
+                statusCode = StatusManager.get_status_code("006", statusCodesDataModel)
+
+            # 100
+            # Check retry count
+            if retry_count > int(itemResponse["config"]["default_retry_count"]):
+                #item_dict["status"]["code"] = "100"
+                statusCode = StatusManager.get_status_code("100", statusCodesDataModel)
+
+            # 101
+            # Check elapsed time
+            avg_elapsed_time_threshold = float(value["average_elapsed_time_factor"]) * float(elapsedTimeAverage)
+            if elapsedTime > avg_elapsed_time_threshold:
+                print(f"\telapsed time threshold: {avg_elapsed_time_threshold}")
+                #item_dict["status"]["code"] = "101"
+                statusCode = StatusManager.get_status_code("101", statusCodesDataModel)
+
+            #statusCode = StatusManager.get_status_code(item_dict["status"]["code"], statusCodesDataModel)
+            LoggingUtils.log_status_code_details(statusCode)
+
+        else:
+            # If we are at this point, then one or more of the Service
+            # states has failed
+            #
+            # The any() function returns True if any item in an iterable
+            # are true, otherwise it returns False.
+            if any([agol_is_valid, item_is_valid, service_is_valid]):
+
+                if service_is_valid:
+                    print(f"Service | Success")
+                    if item_is_valid:
+                        print(f"Item | Success | AGOL must be down, then why is the item accessible?")
+                    else:
+                        # 102
+                        statusCode = StatusManager.get_status_code("102", statusCodesDataModel)
+                        #item_dict["status"]["code"] = "102"
+                        LoggingUtils.log_status_code_details(statusCode)
+
+                    # 201 Check
+                    if layers_are_valid is not True:
+                        statusCode = StatusManager.get_status_code("201", statusCodesDataModel)
+                        #item_dict["status"]["code"] = "201"
+                        LoggingUtils.log_status_code_details(statusCode)
+                else:
+                    # 500
+                    if item_is_valid:
+                        statusCode = StatusManager.get_status_code("500", statusCodesDataModel)
+                        #item_dict["status"]["code"] = "500"
+                        LoggingUtils.log_status_code_details(statusCode)
+                    else:
+                        print(f"Item | Fail")
+                        # If ALL of the Service states are False, we have reached
+                        # a critical failure in the system
+                        statusCode = StatusManager.get_status_code("501", statusCodesDataModel)
+                        #item_dict["status"]["code"] = "501"
+                        LoggingUtils.log_status_code_details(statusCode)
+            else:
+                # If ALL of the Service states are False, we have reached
+                # a critical failure in the system
+                statusCode = StatusManager.get_status_code("501", statusCodesDataModel)
+                #item_dict["status"]["code"] = "501"
+                LoggingUtils.log_status_code_details(statusCode)
         print()
 
 
 
 
 
-    print("\n=================================================================")
-    print(f"Current data and time")
-    print("===================================================================")
-    # get the current date and time
-    timeUtilsResponse = TimeUtils.get_current_time_and_date()
-    timestamp = timeUtilsResponse["timestamp"]
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # Create a new directory to hold the rss feeds (if it does not exist)
     rssDirPath = os.path.realpath(ROOT_DIR + r"\rss")
